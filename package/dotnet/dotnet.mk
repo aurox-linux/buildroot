11aa99f94188d0b2c9e01b0db3f3863f5b436886
vddCore <vddcore@outlook.com>
2026-09-04 03:27:43 +0200

    update .net to 10.0.11

--- package/dotnet/dotnet.mk
+++ package/dotnet/dotnet.mk
@@ -16,7 +16,7 @@ ifeq ($(BR2_x86_64),y)
 	DOTNET_ARCH=x64
 endif
 
-DOTNET_VERSION = 10.0.0-rc.2.25502.107
+DOTNET_VERSION = 10.0.11
 DOTNET_SOURCE = dotnet-runtime-$(DOTNET_VERSION)-linux-$(DOTNET_ARCH).tar.gz
 DOTNET_SITE = https://builds.dotnet.microsoft.com/dotnet/Runtime/$(DOTNET_VERSION)
 DOTNET_INSTALL_TARGET = YES
